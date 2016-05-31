0f5e314e1f211b7ba8dd455e761f1895b3e54475
Sukrit Chhabra <sukrit.chhabra@gmail.com>
2016-05-31 01:21:19 -0400

    Adding function to highlight and basic functionality

---
+++ README.md
@@ -0,0 +1,3 @@
+## Wikipedia Link Highlighter
+
+My First attempt at a chrome extension. Parses ggogle search results and highlights Wikipedia links.
---
+++ content.js
@@ -0,0 +1,31 @@
+/**
+ * Parses all search results and highlights wikipedia links
+ */
+function highlightWikiLinks() {
+    var listSuggestions = $('.g .rc');
+
+    for (var i = 0; i < listSuggestions.length; i++) {
+        var res = listSuggestions[i];
+        var cite_url = $(res).find('.s div.f.kv._SWb ._Rm').text();
+        if (cite_url.indexOf('wikipedia.') >= 0) {
+            $(res).css({
+                'border-left': 'solid 2px green',
+                'padding-left': '1em'
+            });
+        }
+    };
+}
+
+highlightWikiLinks(); // If link opened already has a wiki result
+
+$('body').on('keydown', '.gsfi', function(event) {
+    highlightWikiLinks();
+    if ( event.which === 13 ) {
+        console.log('pressed enter');
+        highlightWikiLinks();
+    }
+});
+
+$('.gsfi').on('keydown', function(event) {
+    highlightWikiLinks();
+});
